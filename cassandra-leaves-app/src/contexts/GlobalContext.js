8eb3dcd3ece9632ec079737c9b78dc300901e346
AnaChacharska <117314692+AnaChacharska@users.noreply.github.com>
2025-01-03 09:44:49 +0100

    Enhance GlobalContext with improved state management

--- cassandra-leaves-app/src/contexts/GlobalContext.js
+++ cassandra-leaves-app/src/contexts/GlobalContext.js
@@ -1,9 +1,9 @@
-import React, { createContext, useState } from 'react';
+import React, { createContext, useState } from "react";
 
 export const GlobalContext = createContext();
 
-export const GlobalProvider = ({ children }) => {
-    const [leaves, setLeaves] = useState([]);
+export const GlobalProvider = ({ children, initialLeavesData = [] }) => {
+    const [leaves, setLeaves] = useState(initialLeavesData);
 
     return (
         <GlobalContext.Provider value={{ leaves, setLeaves }}>
